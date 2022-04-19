eee071e01119c96b6a591c72d1f01d6e54a578b6
jiangyi <189363638@qq.com>
2022-04-19 17:05:23 +0800

    feat: elementplus自动导入集成

--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>1</div>
+  <div ref="value">
+    <el-input />
+    <el-button @click="change">点击</el-button>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   setup() {
-    return {};
+    let value = ref(null);
+    const change = () => {
+      console.log(value.value);
+    };
+    return {
+      value,
+      change
+    };
   }
 });
 </script>
